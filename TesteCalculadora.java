1924e5156938b2612d081645cb9c1bd3b976156a
GCC20000 <93943048+GCC20000@users.noreply.github.com>
2025-11-26 21:57:22 -0300

    Add files via upload

---
+++ TesteCalculadora.java
@@ -0,0 +1,17 @@
+package calculadora;
+
+public class TesteCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+       //Testes Rápidos
+       calc.calc(2, 3, "+") ;  // Esperado: 5
+       calc.calc(10, 4, "-") ;  // Esperado: 6   
+       calc.calc(3, 5, "*") ;  // Esperado: 15       
+       calc.calc(8, 2, "/") ;  // Esperado: 4
+       calc.calc(8, 0, "/") ; // Esperado: Erro divisão por zero         
+       calc.calc(5, 5, "x") ;  // Esperado: Operação inválida
+
+     }
+}
